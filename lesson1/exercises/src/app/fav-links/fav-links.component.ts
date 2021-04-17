898b11d56099f6a2fe0d04972579d2f206b4037f
SandeepKumar1995 <54086388+SandeepKumar1995@users.noreply.github.com>
2021-04-17 03:16:21 -0400

    Updated the files as per the assignment

--- lesson1/exercises/src/app/fav-links/fav-links.component.ts
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-fav-links',
+  selector: 'fav-links',
   templateUrl: './fav-links.component.html',
   styleUrls: ['./fav-links.component.css']
 })
 export class FavLinksComponent implements OnInit {
-
+        favLinks = ['https://www.netflix.com/', 'https://unsplash.com'];
   constructor() { }
 
   ngOnInit() {
